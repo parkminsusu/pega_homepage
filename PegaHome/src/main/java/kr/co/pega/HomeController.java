01aa74fc808a483545e9347c52dd4ea1fc6f0e33
Park <Park@Park-PC>
2015-09-09 20:39:08 +0900

    commit 확인

--- PegaHome/src/main/java/kr/co/pega/HomeController.java
+++ PegaHome/src/main/java/kr/co/pega/HomeController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import kr.co.pega.user.UserService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import kr.co.pega.user.User;
-import kr.co.pega.user.UserService;
-
 /**
- * Handles requests for the application home page.
+ * Handles requests for the application home page
  */
 @Controller
 public class HomeController {
